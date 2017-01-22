a9e585ff64b211543bc13ad52c969fc9335ab220
ZenStein <shaneenterprises@gmail.com>
2017-01-22 07:24:15 -0800

    before app restructuring

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -7,17 +7,34 @@ class StorePicker extends React.Component{
   //  super()
   //  this.goToStore = this.goToStore.bind(this)
  // }
+  componentDidMount(){
+    base.onAuth((user) => {
+    if (user) {
+      console.log('user', user);
+      console.log(this.email.value)
+      console.log(this.password.value)
+    } else {
+      console.log("no user");
+    }
+    })
+  }
   attemptLogin(event){
     event.preventDefault()
     console.log('you hit it')  
-    console.log(this.email.value)
-    console.log(this.password.value)
+    console.log(this.state)
    base.authWithPassword({
       email    : this.email.value,
       password :this.password.value   
    }, (err, user) => {
-     if(user){  }
-     if(err){}
+     if(user){ 
+      this.setState({uid: user.uid})
+    console.log(this.state)
+      console.log(user.uid)
+      this.context.router.transitionTo('/')
+     }
+     if(err){
+      console.log(err)
+     }
     });
   }
   render(){
